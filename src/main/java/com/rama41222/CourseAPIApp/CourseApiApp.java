8a08df27227b57cf7029f548e9c612b412c6e803
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2017-05-28 01:01:55 +0530

    modified the pom to generate war file so that it can be deployed to wso2as

--- src/main/java/com/rama41222/CourseAPIApp/CourseApiApp.java
+++ src/main/java/com/rama41222/CourseAPIApp/CourseApiApp.java
@@ -2,10 +2,17 @@ package com.rama41222.CourseAPIApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseApiApp {
+public class CourseApiApp extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CourseApiApp.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApp.class, args);
 
